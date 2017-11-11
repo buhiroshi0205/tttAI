94a765922f61fbe943fa203715926dc89c570d18
buhiroshi0205 <buhiroshi@hotmail.com>
2017-11-11 22:57:56 +0900

    Added a script for playing against the AI to load different models, change the model.load_weights('model1') line.

--- manvsmachine.py
+++ manvsmachine.py
@@ -1,4 +1,7 @@
-import keras
+from __future__ import print_function
+
+import keras, ttt
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
@@ -10,19 +13,30 @@ model.add(Dense(27, activation='relu'))
 model.add(Dense(27, activation='relu'))
 model.add(Dense(9, activation='softmax'))
 model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.1))
-model.load_weights('gay.h5')
+model.load_weights('model1.h5')
 
-num_weights = input('how many games do you want to play?')
-turn = input('which player do you want to play? (1 or 2)')
+num_games = input('how many games do you want to play?')
 
-for episode in range(num_weights):
+for episode in range(num_games):
+	player = input('which player do you want to play? (1 or -1)')
 	ttt.initialize_game()
 	game_state = ttt.check_game()
+	turn = player
 	while not game_state[0]:
-		board = ttt.get_board()
-		move_probs = model.predict_on_batch(np.array([board]))[0]
-		#move = np.random.choice(9, p=move_probs)
-		move = np.argmax(move_probs)
-		ttt.move(move)
+		if turn == 1:
+			print(np.reshape(ttt.board, (3,3)))
+			move = input('make your move.') - 1
+			ttt.move(move)
+		elif turn == -1:
+			board = ttt.get_board()
+			move_probs = model.predict_on_batch(np.array([board]))[0]
+			move = np.argmax(move_probs)
+			ttt.move(move)
 		game_state = ttt.check_game()
-	print "game %d done!" % episode
+		turn *= -1
+	if game_state[1]*player == 1:
+		print("you win!")
+	else:
+		print("you lose!")
+	print(ttt.moves)
+
