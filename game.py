6513543654ff3eb7d6f4e82065034bac95d360b1
dlwocks <frank_smic@hotmail.com>
2017-11-20 23:18:28 +0900

    game.py: more beautiful check_game

--- game.py
+++ game.py
@@ -29,8 +29,8 @@ class TTT():
 		return True
 
 	def check_game(self):
-		over     =     0 != self.board[4] == self.board[0] == self.board[8] or 0 != self.board[4] == self.board[1] == self.board[7] or 0 != self.board[4] == self.board[2] == self.board[6] or 0 != self.board[4] == self.board[3] == self.board[5]
-		return over or 0 != self.board[0] == self.board[1] == self.board[2] or 0 != self.board[0] == self.board[3] == self.board[6] or 0 != self.board[8] == self.board[2] == self.board[5] or 0 != self.board[8] == self.board[6] == self.board[7]
+		checkls = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+		return any(map(lambda a,b,c: 0 != self.board[a] == self.board[b] == self.board[c], checkls))
 
 	def get_input_board(self):
 		newboard = self.board*self.turn
